c3311037bf28ee98a6b9240ad3b3103fc01de104
master <paulasofiagodoy@gmail.com>
2023-03-03 14:42:57 -0500

    dolar

---
+++ dolar.py
@@ -0,0 +1,19 @@
+import json
+import boto3
+from urllib.request import urlopen
+
+def lambda_handler(event, context):
+    with urlopen("https://totoro.banrep.gov.co/estadisticas-economicas/rest/consultaDatosService/consultaMercadoCambiario") as response:
+        body = response.read() 
+    
+    print(body)
+    
+    s3 = boto3.resource('s3')
+    object=s3.Object('dolar-raw-127', 'dolar_timestamp.txt')
+    object.put(Body=body)
+    print("file uploaded successfully")
+    
+    return {
+    'statusCode': 200,
+    'body': json.dumps('Hello from Lambda!')
+    }
